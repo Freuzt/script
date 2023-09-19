83a383088bd1bf5dfff97ce0bc9db70fca14e799
Freuzt <145448107+Freuzt@users.noreply.github.com>
2023-09-19 12:17:38 +0200

    Create install_apache.sh
    why not

---
+++ install_apache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Mise à jour du système
+sudo apt-get update
+
+# Installation d'Apache
+sudo apt-get install -y apache2
+
+# Démarrage du service Apache
+sudo systemctl start apache2
+
+# Activer Apache pour qu'il démarre automatiquement au démarrage
+sudo systemctl enable apache2
+
+# Configuration du pare-feu pour permettre le trafic HTTP
+sudo ufw allow 'Apache'
